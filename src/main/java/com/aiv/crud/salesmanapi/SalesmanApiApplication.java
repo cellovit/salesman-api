b9a7dcebe465d8cfc0ff19ed247025ec6c8f54fd
cellovit <marcellov1t@hotmail.com>
2023-04-21 13:31:09 -0300

    first commit

---
+++ src/main/java/com/aiv/crud/salesmanapi/SalesmanApiApplication.java
@@ -0,0 +1,13 @@
+package com.aiv.crud.salesmanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesmanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesmanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiv/crud/salesmanapi/SalesmanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiv.crud.salesmanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesmanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
